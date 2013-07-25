235348b3ceed8d9b0c0c9655fcadb7b73930fb50
Satoshi Ebisawa <e.satoshi@gmail.com>
2013-07-25 20:56:32 +0900

    Add URL field

--- app/main_controller.rb
+++ app/main_controller.rb
@@ -2,10 +2,15 @@ class MainController < NSWindowController
   extend IB
 
   outlet :load_button, NSButton
+  outlet :url_field, NSTextField
   outlet :webview, WebView
 
+  def windowDidLoad
+    url_field.setStringValue('http://www.google.com/')
+  end
+
   def load(sender)
-    request = NSURLRequest.requestWithURL(NSURL.URLWithString('http://www.google.com/'))
+    request = NSURLRequest.requestWithURL(NSURL.URLWithString(url_field.stringValue))
     webview.mainFrame.loadRequest(request)
   end
 end
