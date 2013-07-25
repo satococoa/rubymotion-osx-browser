1b72698d64e8dd20fad44f79b886e66fd2ac8099
Satoshi Ebisawa <e.satoshi@gmail.com>
2013-07-25 21:27:05 +0900

    View source

--- app/main_controller.rb
+++ app/main_controller.rb
@@ -6,11 +6,23 @@ class MainController < NSWindowController
   outlet :webview, WebView
 
   def windowDidLoad
-    url_field.setStringValue('http://www.google.com/')
+    url_field.setStringValue('http://www.yahoo.co.jp/')
+    load(self)
   end
 
   def load(sender)
     request = NSURLRequest.requestWithURL(NSURL.URLWithString(url_field.stringValue))
     webview.mainFrame.loadRequest(request)
   end
+
+  # WebView delegate methods
+  def webView(webview, didFinishLoadForFrame: frame)
+    puts "\e[31m======================================================\e[0m"
+    puts webview.mainFrameURL
+    puts "\e[31m======================================================\e[0m"
+    puts webview.stringByEvaluatingJavaScriptFromString("document.getElementsByTagName('html')[0].innerHTML").split(/\n/)[30..50]
+    puts "\e[32m======================================================\e[0m"
+  end
+  # // WebView delegate methods
+
 end
